6e72a176e5d0a0e8f9599985dc10aad8bc900c6c
Jaswanth909 <jaswanth.2939@gmail.com>
2026-02-17 11:30:11 +0530

    Add Java Product class for backend model

---
+++ backend/src/main/java/com/doorstep/Product.java
@@ -0,0 +1,98 @@
+package com.doorstep;
+
+import java.io.Serializable;
+
+/**
+ * Product class representing an ecommerce product
+ */
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private int id;
+    private String name;
+    private double price;
+    private String category;
+    private String description;
+    private int stock;
+    private String imageUrl;
+    
+    // Constructor
+    public Product() {}
+    
+    public Product(int id, String name, double price, String category, String description, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.stock = stock;
+    }
+    
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public int getStock() {
+        return stock;
+    }
+    
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", stock=" + stock +
+                '}';
+    }
+}
